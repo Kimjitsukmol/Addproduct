d3f16a784fe6e4a6d91f7d4d3d3af3e58f48460b
Kimjitsukmol <ice.kanjanawat@gmail.com>
2025-04-10 00:30:05 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const products = [];
+const blob = new Blob(["\uFEFF" + jsonStr], { type: "application/json;charset=utf-8;" });
+
 function addProduct() {
     const id = document.getElementById("productId").value.trim();
     const name = document.getElementById("productName").value.trim();
